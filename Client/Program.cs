98dbfa7b7b4e7060c9994ae72d84a32792ee6c3e
Steliyan Stoyanov <steliyan@st6.io>
2020-07-20 14:00:00 +0300

    Wire-up client to RabbitMQ to Client

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using MassTransit;
+using MassTransit.Util;
 
 namespace Client
 {
@@ -6,7 +8,43 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var bus = CreateBus();
+
+            try
+            {
+                TaskUtil.Await(() => bus.StartAsync());
+
+                while (true)
+                {
+                    try
+                    {
+                        Console.Write("Enter something (quit exits): ");
+                        var line = Console.ReadLine();
+                        if (line == "quit")
+                        {
+                            break;
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Error occurred: {ex}");
+                    }
+                }
+            }
+            finally
+            {
+                TaskUtil.Await(() => bus.StopAsync());
+            }
+        }
+
+        private static IBusControl CreateBus()
+        {
+            return Bus.Factory.CreateUsingRabbitMq(cfg =>
+                cfg.Host(new Uri("rabbitmq://localhost/test"), h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                }));
         }
     }
 }
